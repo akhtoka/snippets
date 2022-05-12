b92fe3d4ee4ecfb02d06b35f9207f9f92ff8a90e
Akihito OKA <akihito2301@gmail.com>
2022-05-12 16:04:19 +0900

    Update analysis_snippets.py

--- analysis_snippets.py
+++ analysis_snippets.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sn
+import seaborn as sns
+from sklearn.metrics import mean_squared_error
 from sklearn.metrics import confusion_matrix
-from sklearn.cluster import KMeans
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model import LassoCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
+
 from imblearn.ensemble import BalancedRandomForestClassifier
 from IPython.display import display
 
 
 def hml_kmeans(clst_list, n_clst):
-    # HML用のkmeansクラスタリング
-    # クラスタ結果の表示、クラスタ番号を平均の高い順に変更する
+    """
+    HML用のkmeansクラスタリング
+    クラスタ結果の表示、クラスタ番号を平均の高い順に変更する
+    """
 
     RANDOM_STATE = 123
     vector_array = np.array(clst_list)
@@ -35,9 +41,9 @@ def hml_kmeans(clst_list, n_clst):
 
 
 def print_cmx(y_true, y_pred, labels):
-
-    # めっちゃ綺麗な混合行列を出力する関数
-    # ヒートマップ機能付き
+    """
+    混合行列をヒートマップで出力する関数
+    """
 
     cmx_data = confusion_matrix(y_true, y_pred, labels=labels)
     print(cmx_data)
@@ -45,7 +51,7 @@ def print_cmx(y_true, y_pred, labels):
 
     plt.figure(figsize=(6, 4.5))
     plt.rcParams['font.size'] = 15
-    sn.heatmap(df_cmx, cmap='Reds', annot=True, fmt="d", xticklabels=True, yticklabels=True)
+    sns.heatmap(df_cmx, cmap='Reds', annot=True, fmt="d", xticklabels=True, yticklabels=True)
     # plt.savefig('heatmap.png')
     ax = plt.gca()
     ax.xaxis.set_ticks_position("top")  # x軸目盛りは軸の上、bottomで下になる
@@ -56,9 +62,10 @@ def print_cmx(y_true, y_pred, labels):
 
 
 def RFClassifier(df, target_column, feature_columns, test_rate):
-
-    # クラス分類用ランダムフォレスト
-    # 混合行列や重要度の高い変数を可視化する
+    """
+    クラス分類用ランダムフォレスト
+    混合行列や重要度の高い変数を可視化する
+    """
 
     # 説明変数、目的変数の作成
     X = df.loc[:, feature_columns].values
@@ -121,11 +128,6 @@ def lassocv_reg(df, x_vars, y_var, test_size=0.2):
     """
     instant Lasso Regression CV
     """
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.linear_model import LassoCV
-    from sklearn.model_selection import train_test_split
-    from sklearn.metrics import mean_squared_error
-    import numpy as np
     
     X = df[x_vars]
     y = df[y_var]
@@ -205,9 +207,10 @@ def simple_dtree(df, x_list, y_var, max_depth=3, regressor=False, min_samples_sp
 
 
 def BalancedRFClassifier(df, target_column, feature_columns, test_rate):
-
-    # 不均衡クラス分類用ランダムフォレスト
-    # 混合行列や重要度の高い変数を可視化する
+    """
+    不均衡クラス分類用ランダムフォレスト
+    混合行列や重要度の高い変数を可視化する
+    """
 
     # 説明変数、目的変数の作成
     X = df.loc[:, feature_columns].values
