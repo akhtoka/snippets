da5fcfe781022048d77277d185805201a39fa1ff
Akihito OKA <akihito2301@gmail.com>
2022-05-12 18:30:09 +0900

    Update analysis_snippets.py

--- analysis_snippets.py
+++ analysis_snippets.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_squared_error, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.grid_search import GridSearchCV
